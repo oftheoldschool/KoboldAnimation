e1ca0488efb4560cd71269f6b75b509cd2db4381
Andy Stanton <andystanton@users.noreply.github.com>
2025-11-22 09:19:17 +0000

    Allow ability to override animation looping

--- Sources/KoboldAnimation/State/AnimationState.swift
+++ Sources/KoboldAnimation/State/AnimationState.swift
@@ -1,11 +1,12 @@
 public class AnimationState {
     let defaultClip: String
     let defaultOffset: Float
-    let playbackSpeed: Float
     let clips: [String: KAClip]
     public let skeleton: KASkeleton
     public let pose: KAPose
 
+    private var playbackSpeed: Float
+    private var looping: Bool
     private var currentClipName: String
     private var animationTime: Float
 
@@ -14,7 +15,8 @@ public class AnimationState {
         clips: [String: KAClip],
         playbackSpeed: Float = 1,
         defaultClip: String? = nil,
-        defaultOffset: Float = 0
+        defaultOffset: Float = 0,
+        looping: Bool = true
     ) {
         self.defaultClip = defaultClip ?? clips.keys.sorted().first!
         self.defaultOffset = defaultOffset
@@ -24,21 +26,29 @@ public class AnimationState {
         self.currentClipName = self.defaultClip
         self.animationTime = 0
         self.playbackSpeed = playbackSpeed
+        self.looping = looping
         self.resetAnimation()
     }
 
     public func animate(_ deltaTime: Float) {
         if let clip = clips[currentClipName] {
-            animationTime = clip.sample(pose: pose, inTime: animationTime + deltaTime * playbackSpeed)
+            animationTime = clip.sample(
+                pose: pose,
+                inTime: animationTime + deltaTime * playbackSpeed,
+                overrideClipLooping: looping)
         }
     }
 
     public func changeClip(
         clipName: String,
-        startOffset: Float = 0
+        startOffset: Float = 0,
+        playbackSpeed: Float = 1,
+        looping: Bool = true
     ) {
-        currentClipName = clipName
-        animationTime = startOffset
+        self.currentClipName = clipName
+        self.animationTime = startOffset
+        self.playbackSpeed = playbackSpeed
+        self.looping = looping
         animate(0)
     }
 
@@ -47,6 +57,10 @@ public class AnimationState {
     }
 
     public func resetAnimation() {
-        changeClip(clipName: defaultClip, startOffset: defaultOffset)
+        changeClip(
+            clipName: defaultClip,
+            startOffset: defaultOffset,
+            playbackSpeed: 1,
+            looping: true)
     }
 }
